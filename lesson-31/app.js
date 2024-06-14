9f986d822aa3a1a77664015a1b9d349ba0cb3fba
taeden211 <162815730+taeden211@users.noreply.github.com>
2024-06-14 14:29:13 +0000

    lesson-31 !

--- lesson-31/app.js
+++ lesson-31/app.js
@@ -96,6 +96,7 @@ app.use((req, res, next) => {
  */
 
 
+// 데이터베이스 연결 설정
 const mongoose = require("mongoose"); // mongoose를 요청
 // 데이터베이스 연결 설정
 mongoose.connect(
---
+++ lesson-31/controllers/chatController.js
@@ -0,0 +1,54 @@
+// controllers/chatController.js
+"use strict";
+
+const Message = require("../models/Message");
+
+/**
+ * Listing 30.4 (p. 445)
+ * chatController.js에서의 채팅 소켓 커넥션 처리
+ */
+module.exports = (io) => {
+  io.on("connection", (socket) => {
+    console.log("User connected");
+
+    /**
+     * Listing 31.11 (p. 460)
+     * 최근 메시지 읽어들이기
+     */
+    Message.find({})
+    .sort({ createdAt: -1 })
+    .limit(10)
+    .then(messages => {
+      messages.reverse().forEach(msg => {
+        io.emit("message", message);
+      });
+    })
+    .catch(error => {
+      console.log(`error: ${error.message}`);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("User disconnected!");
+    });
+
+    /**
+     * Listing 31.2 (p. 451)
+     */
+    socket.on("message", data => {
+      let msgAttr = {
+        content: data.content,
+        userFullName: data.fullName,
+        username: data.username,
+        userId: data.userId
+      };
+      let m = new Message(msgAttr);
+      m.save()
+        .then(() => {
+          io.emit("message", msgAttr);
+        })
+        .catch(error => {
+          console.log(`error: ${error.message}`);
+        });
+    });
+  });
+};
